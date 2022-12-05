7328c11edb8bf4d2ab5df1018eb9d87d1ec0f059
Arsen <hoabs@mail.ru>
2022-12-05 13:09:48 +0300

    Added HW_1.3

---
+++ HW_1.3/HW_1.3.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a number");
+int a = int.Parse(Console.ReadLine());
+
+if(a%2==0)
+{
+    Console.WriteLine("Even number");
+}
+else
+{
+    Console.WriteLine("Odd number");
+}
