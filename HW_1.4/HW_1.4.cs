7a17ed3b7f7df33085689f0bf9ae1ae34009613e
Arsen <hoabs@mail.ru>
2022-12-05 13:43:04 +0300

    Added HW_1.4

---
+++ HW_1.4/HW_1.4.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter a number");
+int a = int.Parse(Console.ReadLine());
+
+if (a>0)
+{
+    string result = "";
+    for(int i=1;i<=a;i++)
+    {
+        if(i%2==0)
+        {
+            result = result + $"{i}; ";
+        }
+        
+    }
+    Console.WriteLine(result);
+}
+else if (a<=0)
+{
+    string result = "";
+    for(int i=1;i>=a;i--)
+    {
+        if(i%2==0)
+        {
+            result = result + $"{i}; ";
+        }
+        
+    }
+    Console.WriteLine(result);
+}
