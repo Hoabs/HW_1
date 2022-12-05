2c7086e38ef934f4439bc46a45e890f8b4d70c68
Arsen <hoabs@mail.ru>
2022-12-05 12:23:46 +0300

    Added Homework 1.1

---
+++ HW_1.1.cs
@@ -0,0 +1,16 @@
+﻿int a = 1, b = 2;
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"max = {b}");
+}
+else
+{
+    Console.WriteLine("a = b");
+}
+
+
