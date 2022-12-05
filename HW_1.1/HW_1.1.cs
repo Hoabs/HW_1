da7d866cb1c8af24f21b04909b6cc429a25d1413
Arsen <hoabs@mail.ru>
2022-12-05 12:31:29 +0300

    Fixed architecture

---
+++ HW_1.1/HW_1.1.cs
@@ -0,0 +1,14 @@
+﻿int a = 1, b = 2;
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"max = {b}");
+}
+else
+{
+    Console.WriteLine("a = b");
+}
